fdfef305d004c582e75f682b2fafb8fa69152367
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-06-05 13:00:09 +0530

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Route, Switch } from "react-router-dom"
+import Home from "./containers/Home"
+import Signin from "./containers/Signin"
+import Signup from "./containers/Signup"
 
 /**
 * @author
@@ -6,10 +10,15 @@ import React from 'react'
 **/
 
 const App = (props) => {
-  return(
-    <div>App</div>
-   )
-  }
-
+  return (
+    <div>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/Signin' component={Signin} />
+        <Route path='/Signup' component={Signup} />
+      </Switch>
+    </div>
+  )
+}
 
 export default App
